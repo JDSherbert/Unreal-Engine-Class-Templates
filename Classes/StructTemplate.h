ed681201889101ad9bcdad9f86c9ece57ca5c385
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-15 19:30:11 +0000

    Update StructTemplate.h

--- Classes/StructTemplate.h
+++ Classes/StructTemplate.h
@@ -1,17 +1,26 @@
 // ©2023 JDSherbert. All rights reserved.
 
+#pragma once
+
 #include <Runtime/Core/Public/CoreMinimal.h>
 
 #include "StructTemplate.generated.h"
 
+/**
+ * Struct Template class
+ * \n
+ * Description goes here!
+ * @since Unreal Engine 5.2
+ * @author JDSherbert
+ */
 USTRUCT(Blueprintable, Category = "SomeCategory|SomeSubCategory")
 struct FStructTemplate
 {
-	  GENERATED_BODY();
+    GENERATED_BODY();
 
 public:
 
-	  FStructTemplate()
+    FStructTemplate()
     : SomeInt(0)
     , SomeFloat(0.0f)
     {
@@ -23,9 +32,9 @@ public:
         // Assign destructor values here
     }
 
-	  UPROPERTY(BlueprintReadWrite, Category = "SomeCategory|SomeSubCategory")
-	  int SomeInt;
+    UPROPERTY(BlueprintReadWrite, Category = "SomeCategory|SomeSubCategory")
+    int SomeInt;
 
     UPROPERTY(BlueprintReadWrite, Category = "SomeCategory|SomeSubCategory")
-	  float SomeFloat;
+    float SomeFloat;
 };
