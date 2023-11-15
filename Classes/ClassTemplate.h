d522cce965b779dbbf10cea6b9c5371c80912053
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-15 17:21:29 +0000

    Update ClassTemplate.h

--- Classes/ClassTemplate.h
+++ Classes/ClassTemplate.h
@@ -1,1 +1,34 @@
+// ©2023 JDSherbert. All rights reserved.
 
+#include <Runtime/Core/Public/CoreMinimal.h>
+
+#include "ClassTemplate.generated.h"
+
+USTRUCT(Blueprintable, Category = "SomeCategory|SomeSubCategory")
+struct UClassTemplate
+{
+	  GENERATED_BODY();
+
+public:
+
+	  UClassTemplate()
+    : SomeInt(0)
+    , SomeFloat(0.0f)
+    {
+        // Assign constructor values here
+    }
+
+    ~FStructTemplate()
+    {
+        // Assign destructor values here
+    }
+
+	  UPROPERTY(BlueprintReadWrite, Category = "SomeCategory|SomeSubCategory")
+	  int SomeInt;
+
+    UPROPERTY(BlueprintReadWrite, Category = "SomeCategory|SomeSubCategory")
+	  float SomeFloat;
+
+    UFUNCTION(BlueprintCallable)
+    FORCEINLINE void SomeFunction() const { return; }
+};
