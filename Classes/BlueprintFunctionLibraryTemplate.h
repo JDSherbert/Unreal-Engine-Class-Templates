4b94d7a68b5624d814f2b857deb5c74832ba0560
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-15 19:22:26 +0000

    Update BlueprintFunctionLibraryTemplate.h

--- Classes/BlueprintFunctionLibraryTemplate.h
+++ Classes/BlueprintFunctionLibraryTemplate.h
@@ -1,1 +1,33 @@
+// ©2023 JDSherbert. All rights reserved.
+
+#pragma once
+
+#include <Runtime/Core/Public/CoreMinimal.h>
+
+#include <Runtime/Engine/Classes/Kismet/BlueprintFunctionLibrary.h>
+
+#include "BlueprintFunctionLibraryTemplate.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSherbert_Tools, Log, All);
+
+/**
+ * Blueprint Function Library Template class
+ * \n
+ * Description goes here!
+ * @since Unreal Engine 5.2
+ * @author JDSherbert
+ */
+UCLASS()
+class TEMPLATE_API UBlueprintFunctionLibraryTemplate final : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    /**
+    *
+    */
+    UFUNCTION(BlueprintCallable, Category = "SomeCategory|SomeSubCategory")
+    static void SomeStaticFunction();
+};
 
